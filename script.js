6842ac7f55b18aa01f9c51c9697a4f93f1592331
iceclimberGD <iceclimber999@gmail.com>
2023-06-29 01:29:40 -0400

    add a gui and change the script to update it

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 let playerWins = 0;
 let computerWins = 0;
+let round = 0;
+
+let message = document.getElementById("round-message");
+let gameMessage = document.getElementById("game-message");
+let rockBtn = document.getElementById("rock");
+let paperBtn = document.getElementById("paper");
+let scissorsBtn = document.getElementById("scissors");
+let restartBtn = document.getElementById("restart");
+let playerScore = document.getElementById("play-score");
+let computerScore = document.getElementById("comp-score");
+
+console.log(rockBtn);
+console.log(paperBtn);
+console.log(scissorsBtn);
+
+
+rockBtn.addEventListener("click", function() {
+    playRound("rock");
+  });
+paperBtn.addEventListener("click", function() {
+    playRound("paper");
+  });
+scissorsBtn.addEventListener("click", function() {
+    playRound("scissors");
+  });
+restartBtn.addEventListener("click", function() {
+    restart();
+  });
+
 
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3)) + 1;
@@ -14,61 +43,68 @@ function getComputerChoice() {
     }
 }
 
-function game(){
-    let computerSelection;
-    let playerSelection;
-    let result;
+function playRound(playerSelection){
+    round++;
+    computerSelection = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerSelection);
 
-    for(let i = 1; i < 6; i++){
-        console.log("----Round " + i + "----")
-        computerSelection = getComputerChoice();
-
-        playerSelection = prompt("Rock, Paper, or Scissors?");
-        result = playRound(playerSelection, computerSelection);
-
-        console.log("Your choice: \t\t" + playerSelection);
-        console.log("Computer's choice: \t" + computerSelection);
-        console.log(result);
-        console.log("\n\n");
-    }
-
-    if(playerWins > computerWins){
-        console.log("You won " + playerWins + " game(s) against the computer's " + computerWins + ". You won the match!");
-    }else if(computerWins > playerWins){
-        console.log("You won " + playerWins + " game(s) against the computer's " + computerWins + ". You lost the match...");
-    }else{
-        console.log("You won " + playerWins + " game(s) against the computer's " + computerWins + ". The match was a tie!");
-    }
-
-    playerWins = 0;
-    computerWins = 0;
-}
-
-
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    
     if(playerSelection === computerSelection){
-        return "Round was a tie!";
+        message.innerText = "Round was a tie!";
     }else if(playerSelection === "rock" && computerSelection === "scissors"){
         playerWins++;
-        return "You win! Rock beats Scissors!";
+        message.innerText = "You win! Rock beats Scissors!";
     }else if(playerSelection === "paper" && computerSelection === "rock"){
         playerWins++;
-        return "You win! Paper beats Rock!";
+        message.innerText = "You win! Paper beats Rock!";
     }else if(playerSelection === "scissors" && computerSelection === "paper"){
         playerWins++;
-        return "You win! Scissors beats Paper!";
+        message.innerText = "You win! Scissors beats Paper!";
     }else if(playerSelection === "rock" && computerSelection === "paper"){
         computerWins++;
-        return "You lose! Paper beats Rock!";
+        message.innerText = "You lose! Paper beats Rock!";
     }else if(playerSelection === "paper" && computerSelection === "scissors"){
         computerWins++;
-        return "You lose! Scissors beats Paper!";
+        message.innerText = "You lose! Scissors beats Paper!";
     }else if(playerSelection === "scissors" && computerSelection === "rock"){
         computerWins++;
-        return "You lose! Rock beats Scissors!";
+        message.innerText = "You lose! Rock beats Scissors!";
     }else{
-        return "Invalid input!";
+        message.innerText = "Invalid input!";
     }
+
+    updateScore();
+}
+
+function updateScore(){
+    computerScore.innerText = computerWins;
+    playerScore.innerText = playerWins;
+
+    if(round % 5 == 0){
+        if(playerWins > computerWins){
+            gameMessage.innerText = "You won the match!";
+        }else if(computerWins > playerWins){
+            gameMessage.innerText = "You lost the match...";
+        }else{
+            gameMessage.innerText = "The match was a tie!";
+        }
+
+        rockBtn.setAttribute("disabled", "");
+        paperBtn.setAttribute("disabled", "");
+        scissorsBtn.setAttribute("disabled", "");
+
+        restartBtn.style = "display: block";
+    }
+}
+
+function restart(){
+    playerWins = 0;
+    computerWins = 0;
+    computerScore.innerText = 0;
+    playerScore.innerText = 0;
+
+    rockBtn.removeAttribute("disabled");
+    paperBtn.removeAttribute("disabled");
+    scissorsBtn.removeAttribute("disabled");
+    restartBtn.style = "display: none";
 }
